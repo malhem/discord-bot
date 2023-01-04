2a1e5cfba4ab240eeb3bffeae3a42d5adadd7dfd
Malte Alhem <ma1672al-s@student.lu.se>
2023-01-04 18:43:05 +0100

    blabla

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -14,17 +14,11 @@ namespace DiscordBot
 {
     class Program
     {
-        // There is no need to implement IDisposable like before as we are
-        // using dependency injection, which handles calling Dispose for us.
         static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
-            // You should dispose a service provider created using ASP.NET
-            // when you are finished using it, at the end of your app's lifetime.
-            // If you use another dependency injection framework, you should inspect
-            // its documentation for the best way to do this.
             using (var services = ConfigureServices())
             {
                 var client = services.GetRequiredService<DiscordSocketClient>();
@@ -34,13 +28,14 @@ namespace DiscordBot
                 services.GetRequiredService<CommandService>().Log += LogAsync;
 
                 // Tokens should be considered secret data and never hard-coded.
-                string token = File.ReadAllText(@"C:\Users\malte\Desktop\repos\DiscordBot\token.txt.txt");
+                string token = File.ReadAllText(@"C:\Users\malte\Desktop\repos\DiscordBot\token.txt");
                 await client.LoginAsync(TokenType.Bot, token);
                 await client.StartAsync();
 
                 //Initialize Node connection
                 await audio.InitializeAsync();
-                // Here we initialize the logic required to register our commands.
+                
+                // Initialize the logic required to register our commands.
                 await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
 
                 await Task.Delay(Timeout.Infinite);
